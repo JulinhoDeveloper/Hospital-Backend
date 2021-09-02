a6fda9b63abeaf5017351420d9880cd899ae66ce
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-09-02 18:34:12 -0300

    rotas e controllers para atualizar usuários

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -74,10 +74,63 @@ const  criarUsuario  = async(req, res = response) => {
 
 }
 
+const atualizarUsuario = async (req, res = response) => {
+
+    // TODO: Validar token y comprobar si es el usuario correcto
+
+    const uid = req.params.id;
+
+
+    try {
+
+        const usuarioDB = await Usuario.findById( uid );
+
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        // Actualizaciones
+        const { password, google, email, ...campos } = req.body;
+
+        if ( usuarioDB.email !== email ) {
+
+            const existeEmail = await Usuario.findOne({ email });
+            if ( existeEmail ) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                });
+            }
+        }
+        
+        campos.email = email;
+        const usuarioAtualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
+
+        res.json({
+            ok: true,
+            usuario: usuarioAtualizado
+        });
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Erro inesperado'
+        })
+    }
+
+}
+
+
 
 
 
 module.exports = {
+    getUsuarios,
     criarUsuario,
-    getUsuarios
+    atualizarUsuario
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,7 +5,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const {   getUsuarios, criarUsuario } = require('../controllers/usuarios');
+const {   getUsuarios, criarUsuario, atualizarUsuario } = require('../controllers/usuarios');
 
 
 
@@ -24,6 +24,17 @@ router.post( '/',
     criarUsuario 
 );
 
+
+router.put( '/:id',
+    [
+        
+        check('name', 'O nome é obrigatório').not().isEmpty(),
+        check('password', 'A senha é obrigatoria').not().isEmpty(),
+        check('email', 'O email é obrigatório').isEmail(),
+        validarCampos,
+    ],
+    atualizarUsuario
+);
 
 
 
